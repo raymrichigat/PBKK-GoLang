4c3a3f1ba6b00dd2fb73bf5c49f69106f4e8ebd4
raymrichigat <gentanirwana5@gmail.com>
2024-10-29 17:08:23 +0700

    Update loop

--- main.go
+++ main.go
@@ -1,36 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	)
+	
 
 func main() {
 
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
 	conferenceName := "Go Conference"
+	bookings := []string{}
 
 	fmt.Printf("Welcome to %v booking application.\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Printf("Get your tickets here to attend\n")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+	
+		fmt.Println("Enter Your First Name: ")
+		fmt.Scanln(&firstName)
+	
+		fmt.Println("Enter Your Last Name: ")
+		fmt.Scanln(&lastName)
+	
+		fmt.Println("Enter Your Email: ")
+		fmt.Scanln(&email)
+	
+		fmt.Println("Enter number of tickets: ")
+		fmt.Scanln(&userTickets)
+	
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName+" "+lastName)
+	
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
-	fmt.Println("Enter Your First Name: ")
-	fmt.Scanln(&firstName)
+		firstNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+		fmt.Printf("The first names %v\n", firstNames)
+	}
 
-	fmt.Println("Enter Your Last Name: ")
-	fmt.Scanln(&lastName)
+		}	
 
-	fmt.Println("Enter Your Email: ")
-	fmt.Scanln(&email)
-
-	fmt.Println("Enter number of tickets: ")
-	fmt.Scanln(&userTickets)
-
-	remainingTickets = remainingTickets - userTickets
-
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
-}
